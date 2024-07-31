df9b7d0c57c645ba3507ef18c5b05460a776f489
Tejass <2022bit032.sggs.ac.in>
2024-07-31 22:30:52 +0530

    .

---
+++ validnumber.py
@@ -0,0 +1,18 @@
+def fun(len):
+	if(len>1):
+		if(len):
+			return true;
+			
+		else:
+			q=1;
+	else:
+		return false;
+q=0;		
+a = fun();
+if(a):
+	print('valid number');
+else:
+	print('enter a number greater than or equal to one'); 
+if(q):
+	print('enter positive integer');
+ 	
